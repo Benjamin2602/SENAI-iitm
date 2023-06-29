0ca9a75ae16aecc0497ef2a95e85c4474019dc50
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2023-06-30 01:40:40 +0530

    box model for cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,20 @@
-
+import "../css/mainContent.css";
 const Card = () => {
   return (
     <div>
-      
+      <h1 className="card-heading">More details of today weather</h1>
+      <div className="card-container">
+        <div className="card-items">
+          <div className="card-box card-1"></div>
+          <div className="card-box card-2"></div>
+          <div className="card-box card-3"></div>
+          <div className="card-box card-4"></div>
+          <div className="card-box card-5"></div>
+          <div className="card-box card-6"></div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
