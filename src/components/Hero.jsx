dde1babdad5388e8ffda73ecfde4133b38fbb670
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2023-06-30 19:30:50 +0530

    Update Chart

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,47 +1,55 @@
 import "../css/mainContent.css";
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
-
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 const data = [
   {
-    name: 'Sun',
-    uv: 4000,
+    name: "Sun",
+    uv: 2000,
     pv: 2400,
     amt: 2400,
-  
   },
   {
-    name: 'Mon',
-    uv: 3000,
+    name: "Mon",
+    uv: 2000,
     pv: 1398,
     amt: 2210,
   },
   {
-    name: 'Tue',
-    uv: 2000,
+    name: "Tue",
+    uv: 3000,
     pv: 9800,
     amt: 2290,
   },
   {
-    name: 'Wed',
-    uv: 2780,
+    name: "Wed",
+    uv: 3780,
     pv: 3908,
     amt: 2000,
   },
   {
-    name: 'Thur',
+    name: "Thur",
     uv: 1890,
     pv: 4800,
     amt: 2181,
   },
   {
-    name: 'Fri',
-    uv: 2390,
+    name: "Fri",
+    uv: 4390,
     pv: 3800,
     amt: 2500,
   },
   {
-    name: 'Sat',
-    uv: 3490,
+    name: "Sat",
+    uv: 2490,
     pv: 4300,
     amt: 2100,
   },
@@ -50,28 +58,43 @@ const data = [
 const Hero = () => {
   return (
     <div className="chart-container">
-    <div className="chart-items">
-    <h1 className="chart-heading">Upcoming hours</h1>
-    <ResponsiveContainer width="100%" height={200} className="chart">
-      <AreaChart
-        width={500}
-        height={400}
-        data={data}
-        margin={{
-          top: 10,
-          right: 30,
-          left: 0,
-          bottom: 0,
-        }}
-      >
-        <CartesianGrid vertical={true} horizontal={false}  />
-        <XAxis dataKey="name" />
-        <YAxis dataKey="pv" hide />
-        <Tooltip />
-        <Area type="monotone" dataKey="uv" stroke="#8884d8" strokeWidth={1}  fill="blue" />
-      </AreaChart>
-    </ResponsiveContainer>
-    </div>
+      <div className="chart-items">
+        <h1 className="chart-heading">Upcoming hours</h1>
+
+        <div className="chart-attribute-container">
+          <div className="chart-attribute-item-1">
+            Rain precipitation <KeyboardArrowDownIcon fontSize="inherit" />
+          </div>
+          <div className="chart-attribute-item-2">
+            Next days <ChevronRightIcon fontSize="inherit" />
+          </div>
+        </div>
+        <ResponsiveContainer width="100%" height={200} className="chart">
+          <AreaChart
+            width={500}
+            height={400}
+            data={data}
+            margin={{
+              top: 10,
+              right: 30,
+              left: 0,
+              bottom: 0,
+            }}
+          >
+            <CartesianGrid vertical={true} horizontal={false} />
+            <XAxis dataKey="name" />
+            <YAxis dataKey="pv" hide />
+            <Tooltip />
+            <Area
+              type="monotone"
+              dataKey="uv"
+              stroke="#8884d8"
+              strokeWidth={1}
+              fill="blue"
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 };
